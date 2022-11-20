18be825241e7958c6e8a5d71850e0ca069fbe4fd
david-commit <david.ondiege@students.moringaschool.com>
2022-11-21 00:08:11 +0300

    Completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,24 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  # def index
+    
+  #   query_first_name = params[:first_name] 
+  #   query_last_name = params[:last_name] 
+
+  #  students = if query_first_name
+  #     Student.find_by(first_name: query_first_name)
+  #   elsif query_last_name
+  #     Student.find_by(last_name: query_last_name)
+  #   else
+  #     Student.all
+  #   end
+  #   render json: students
+  # end
+
 end
